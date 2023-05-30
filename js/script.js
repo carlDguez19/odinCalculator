a9e09311ee3659793dbc1d9b6a37973387a7cc85
carlDguez19 <carldominguez19@gmail.com>
2023-05-30 14:29:50 -0600

    I have been writing some pseudoCode that I will try to implement in js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let add = (a, b) => {
+    return a+b;
+}
+let sub = (a, b) => {
+    return a-b;
+}
+let mult = (a, b) => {
+    return a*b;
+}
+let div = (a, b) => {
+    return a/b;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = "+";
