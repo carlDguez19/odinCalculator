8a30805a78cfcbb4882bafa7c58f96539085516c
carlDguez19 <carldominguez19@gmail.com>
2023-05-31 21:20:49 -0600

    added event delegation to capture

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ let op = "";
 let lastPress = "";
 const numRegex = /[0-9]/;
 const symbolRegex = /[+*.-]/;
+execCalc();
 
-const createCalc = () => {
-    
+function execCalc() {
+    let buttons = document.querySelector(".buttonFrame");
+
+    buttons.addEventListener('click', (e) => {
+        if(e.target.tagName === 'DIV'){
+            console.log(e.target.dataset.value);
+        }
+    })
 };
