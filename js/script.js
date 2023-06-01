cdc688d623c2ccfd0b14a62ab8baac6df50fd68c
carlDguez19 <carldominguez19@gmail.com>
2023-05-31 23:05:08 -0600

    added a function to determine which number to fill based on if a checker is set to true or not. Tomorrow I will add the symbol function that will set this checker. and determine if it is the first or nth time a symbol(/*-+) is pressed

--- js/script.js
+++ js/script.js
@@ -2,22 +2,55 @@ let fn = "";
 let sn = "";
 let op = "";
 let lastPress = "";
-const numRegex = '/^\d+$/';
-const symbolRegex = /[+*.-]/;
+let fnCheck =  false;
+// const numRegex = '/^\d+$/'; this will be used as a last resort
+// const symbolRegex = /[+*.-]/;
+fn = "9"  "2";
+// fn += "b";
+// fn += "pleb";
+console.log(fn);
 execCalc();
 
-function execCalc() {
+function execCalc() {//event delegation to determine button clicked
     let buttons = document.querySelector(".buttonFrame");
 
     buttons.addEventListener('click', (e) => {
         if(e.target.tagName === 'DIV'){
             console.log(e.target.dataset.value);
-            if(!isNaN(e.target.dataset.value)){
+            checkIfNum(e.target.dataset.value);
+            if(!isNaN(e.target.dataset.value)){//check if it is a number that is being clicked
                 console.log("its a numbah!!");
+                // here i will determine if firstNumber(fn) or secondNumber(sn) needs to be filled
+                determineNumberFill(e.target.dataset.value);
             }
-            else if(e.target.dataset.value == /[*+/-]/){
+            else{
                 console.log("its a symbul eeee");
+                //here i will determine if it is the first symbol that is clicked or the second, third, etc.
             }
         }
     })
 };
+
+function determineNumberFill(num){
+    if(fnCheck == true){
+        sn += num.toString();
+        lastPress = num;
+    }
+    else{
+        fn += num.toString();
+        lastPress = num;
+    }
+    // if(fn === ""){
+    //     fn = num;
+    //     lastPress = num;
+    // }
+};
+
+// function checkIfNum(num){//test to make sure that the e.target.dataset.value can be passed as a param
+//     if(!isNaN(num)){
+//         console.log("numbah govnah!!!");
+//     }
+//     else{
+//         console.log("symbul govnah!!!");
+//     }
+// }
