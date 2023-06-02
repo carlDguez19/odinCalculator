5c5283bd13efda4337f89971105f1d1e97f2a046
carlDguez19 <carldominguez19@gmail.com>
2023-06-02 13:04:17 -0600

    this should technically be a functioning calculator it just needs to display out to the 'screen' on the calculator. I will write some functions to do this and an extra variable that will keep track of the full arithmetic function

--- js/script.js
+++ js/script.js
@@ -5,10 +5,10 @@ let lastPress = "";
 let fnCheck =  false;
 // const numRegex = '/^\d+$/'; this will be used as a last resort
 // const symbolRegex = /[+*.-]/;
-fn = "9" + "2";
+// fn = +fn + +sn;//testing unary operator
+// console.log(fn);
 // fn += "b";
-// fn += "pleb";
-console.log(fn);
+// fn += "pb";
 execCalc();
 
 function execCalc() {//event delegation to determine button clicked
@@ -17,14 +17,14 @@ function execCalc() {//event delegation to determine button clicked
     buttons.addEventListener('click', (e) => {
         if(e.target.tagName === 'DIV'){
             console.log(e.target.dataset.value);
-            checkIfNum(e.target.dataset.value);
+            //checkIfNum(e.target.dataset.value);
             if(!isNaN(e.target.dataset.value)){//check if it is a number that is being clicked
-                console.log("its a numbah!!");
+                console.log("its a number");
                 // here i will determine if firstNumber(fn) or secondNumber(sn) needs to be filled
                 determineNumberFill(e.target.dataset.value);
             }
             else{
-                console.log("its a symbul eeee");
+                console.log("its a symbol");
                 determineSymbolFill(e.target.dataset.value);
                 //here i will determine if it is the first symbol that is clicked or the second, third, etc.
             }
@@ -33,7 +33,40 @@ function execCalc() {//event delegation to determine button clicked
 };
 
 function equals(){
+    switch(op){
+        case "+":
+            fn = +fn + +sn;//unary operator to do addition
+            sn = "";
+            //display fn as current and append to full arithFunc to display on top left
+            break;
+        case "-":
+            fn = fn - sn;
+            sn = "";
+            //display fn as current and append to full arithFunc to display on top left
+            break;
+        case "*":
+            fn = fn * sn;
+            sn = "";
+            //display fn as current and append to full arithFunc to display on top left
+            break;
+        case "/":
+            fn = fn / sn;
+            sn = "";
+            //display fn as current and append to full arithFunc to display on top left
+            break;
+        case "c":
+            clearCalc();
+            //clearDisplay();
+            break;
+    }
+}
 
+function clearCalc(){
+    lastPress = "";//arithmetic func complete reset everything
+    fnCheck = false;
+    op = "";
+    fn = "";
+    sn = "";
 }
 
 function determineSymbolFill(sym){
@@ -44,11 +77,18 @@ function determineSymbolFill(sym){
         //insert display function(append to display "screen")
     }
     else if(op && !isNaN(lastPress)){//chained operation eg 1+2*3-4/5
-        op = sym;
-        equals();
-        fnCheck = true;
-        lastPress = "sym";
-        //insert display function(append to display "screen")
+        if(op === "="){
+            equals();
+            clearCalc();
+            //display full arithmetic function with result
+        }
+        else{
+            equals();
+            op = sym;
+            fnCheck = true;
+            lastPress = "sym";
+            //insert display function(append to display "screen")
+        }
     }
 }
 
